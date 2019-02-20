c29c7b9e2bbb9d41b2735e365d923c248b1b2e0b
Simone Smith <simonecaplin@gmail.com>
2019-02-20 14:58:58 +0000

    Change handler function to prime number checker

--- src/main/scala/com/gu/microserviceWorkshop/Lambda.scala
+++ src/main/scala/com/gu/microserviceWorkshop/Lambda.scala
@@ -1,18 +1,25 @@
 package com.gu.microserviceWorkshop
 
-import io.circe.syntax._
 import java.io._
 import java.nio.charset.StandardCharsets.UTF_8
 
 object Lambda {
 
+  def isPrime(number: Int): Boolean = {
+    if (number <= 1) {
+      false
+    } else if (number == 2) {
+      true
+    } else {
+      !(2 until number).exists(x => number % x == 0)
+    }
+  }
+
   def handler(in: InputStream, out: OutputStream): Unit = {
+    val jsonPayload = scala.io.Source.fromInputStream(in).mkString("")
+    val result = if (isPrime(jsonPayload.toInt)) "this is a prime number" else "this is not a prime number"
 
-    val response = APIResponse(200,  Map("Content-Type" -> "application/json"), "hello world again")
-
-    //no spaces converts json to a string
-    out.write(response.asJson.noSpaces.getBytes(UTF_8))
-
+    out.write(result.getBytes(UTF_8))
   }
 
 }
